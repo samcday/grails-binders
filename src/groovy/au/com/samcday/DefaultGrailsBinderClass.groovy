c2b1211753866ff0175f8caf96dde55e327f44f8
Sam Day <sam.day@wotifgroup.com>
2012-01-26 02:57:47 +1000

    added interface methods to GrailsBinderClass and implemented them in DefaultGrailsBinderClass

--- src/groovy/au/com/samcday/DefaultGrailsBinderClass.groovy
+++ src/groovy/au/com/samcday/DefaultGrailsBinderClass.groovy
@@ -3,10 +3,32 @@ package au.com.samcday
 import org.codehaus.groovy.grails.commons.AbstractInjectableGrailsClass
 
 class DefaultGrailsBinderClass extends AbstractInjectableGrailsClass implements GrailsBinderClass {
+    static TYPE_PROPERTY = "type"
+
+    static BIND_CLOSURE = "bind"
+    static AS_TEXT_CLOSURE = "bind"
 
     public DefaultGrailsBinderClass(Class clazz) {
         super(clazz, BinderArtefactHandler.TYPE)
 
         println "created binder class"
+    }
+
+    @Override
+    Class getType() {
+        Object obj = getPropertyValue(TYPE_PROPERTY)
+        if(obj == null || !(obj instanceof Class)) return null
+
+        return (Class)obj
+    }
+
+    @Override
+    Object bind(String text) {
+        return metaClass.invokeMethod(referenceInstance, BIND_CLOSURE, [text] as Object[])
+    }
+
+    @Override
+    String asText(Object val) {
+        return metaClass.invokeMethod(referenceInstance, AS_TEXT_CLOSURE, [val] as Object[])
     }
 }
--- src/groovy/au/com/samcday/GrailsBinderClass.groovy
+++ src/groovy/au/com/samcday/GrailsBinderClass.groovy
@@ -3,5 +3,7 @@ package au.com.samcday
 import org.codehaus.groovy.grails.commons.InjectableGrailsClass
 
 public interface GrailsBinderClass extends InjectableGrailsClass {
-
+    public Class getType()
+    public Object bind(String text)
+    public String asText(Object val)
 }
